c4bd12684fe49df0c32493b2b7ecccc7e26e65aa
Lubna Khalid <lkhalid0095@bths.edu>
2022-03-10 01:58:21 -0500

    Finised till C

--- index.js
+++ index.js
@@ -6,46 +6,55 @@ function add() {
     let listItem = document.getElementById("list_item")
     if (input.value == "") {
         alert("Please enter a task before adding")
-    }
-    else {
+    } else {
         let newLi = document.createElement("ul"); //creates elements
         newLi.setAttribute('class', 'list-item')
+        let priority = document.getElementById("priority")
+        let value = priority.options[priority.selectedIndex].value;
+        if(value == ""){
+            newLi.style.color = "black";
+        }
+        else if(value == "high"){
+            newLi.style.color = "red";
+        }
+        else if(value == "medium"){
+            newLi.style.color = "yellow";
+        }
+        else if(value == "low"){
+            newLi.style.color = "blue";
+        }
         let check = document.createElement("input")
         check.type = "checkbox"
         check.onclick = function () {
             if (check.checked) {
+                var audio = new Audio('audio.mp3');
                 newLi.style.textDecoration = "line-through"
+                audio.play();
+                // alert("Congratulations on completing the task!")
+                //decided against using alert because its annoying everytime you click on complete
                 // console.log("striked")
             } else if (!check.checked) {
                 newLi.style.textDecoration = "none"
             }
 
         }
+
         newLi.appendChild(check)
         newLi.appendChild(document.createTextNode(input.value)) //adds the user input to the list
         listItem.appendChild(newLi)
-        let date = document.createElement("input")
-        date.type = "date"
-        date.setAttribute('class', 'date')
-        newLi.appendChild(date)
-        let important = document.createElement("select")
-        var high = document.createElement("option");
-        high.value = "high";
-        high.text = "High Priority";
-        high.setAttribute('class','high')
-        var medium = document.createElement("option");
-        medium.text = "Medium Priority";
-        high.setAttribute('class','med')
-        var low = document.createElement("option");
-        low.text = "Low Priority";
-        high.setAttribute('class','low')
-        important.add(high)
-        important.add(medium)
-        important.add(low)
-        newLi.appendChild(important)
+        let date = document.getElementById("date")
+        let due = document.createElement("p")
+        let text = document.createTextNode("");
+        if(date.value != ""){
+            
+            text = document.createTextNode("Due date is " + date.value);
+        }
+        due.appendChild(text)
+        due.setAttribute('class', 'date')
+        newLi.appendChild(due)
         var deleteButton = document.createElement("button"); //creates a button for delete
         deleteButton.setAttribute('class', 'delete')
-        deleteButton.innerHTML = '<img src = "del.png" height="50%" width="40%" />'
+        deleteButton.innerHTML = '<img src = "del.png" height="40%" width="40%" />'
         // deleteButton.appendChild(document.createTextNode("Delete"));
         newLi.appendChild(deleteButton);
         input.value = "" //for the next input
@@ -54,14 +63,4 @@ function add() {
             newLi.parentNode.removeChild(newLi);
         }
     }
-
-    function strike(check, input) {
-        if (check.checked) {
-            document.write(input.value.strike());
-            console.log("striked")
-        }
-        console.log(check.checked)
-    }
-
-
 }
